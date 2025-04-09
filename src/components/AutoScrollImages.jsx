863ff68493463d3e50c280c945a1eca46e588c4d
bibhu-git <bibhuranjanm324@gmail.com>
2025-04-09 07:42:59 +0530

    fix scroll images on phone screen

--- src/components/AutoScrollImages.jsx
+++ src/components/AutoScrollImages.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import scroll1 from '/scroll1.jpg'
-import scroll3 from '/scroll3.jpg'
-import scroll5 from '/scroll5.jpg'
+import scroll1 from '/scroll1.jpg';
+import scroll3 from '/scroll3.jpg';
+import scroll5 from '/scroll5.jpg';
+
 const images = [scroll1, scroll3, scroll5];
 
 export default function AutoScrollCarousel() {
@@ -28,7 +29,7 @@ export default function AutoScrollCarousel() {
               src={img}
               alt={`Slide ${index + 1}`}
               loading="lazy"
-              className="w-full h-[85vh] sm:h-[75vh] md:h-[80vh] lg:h-[84vh] object-cover rounded-md"
+              className="w-full h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[85vh] object-cover rounded-md"
             />
           </div>
         ))}
